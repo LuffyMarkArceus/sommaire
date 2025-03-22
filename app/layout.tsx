8dacd4aab9ded0e650ef8ad081c6e2435d798b24
shreysaini1998 <shreysaini1998@gmail.com>
2025-03-22 06:04:39 -0500

    Completed Landing Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
+import Header from "../components/common/header";
+import Footer from "@/components/common/footer";
 import "./globals.css";
 
 const fontSans = FontSans({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight : ['200', '300', '400', '500', '600', '700', '800', '900']
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
-
 
 export const metadata: Metadata = {
   title: "Sommaire - AI Powered PDF Summarizer",
-  description: "Save hours of reading time. Transform large PDFs to clear, concise summaries in secondds with AI",
+  description:
+    "Save hours of reading time. Transform large PDFs to clear, concise summaries in secondds with AI",
 };
 
 export default function RootLayout({
@@ -21,10 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${fontSans.variable} font-sans antialiased`}
-      >
-        {children}
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        <div className="realtive flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
